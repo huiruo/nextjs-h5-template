08db5ac8b1470a606910206c68f20e64d92d2f67
ruo <ruomechen@gmail.com>
2023-11-15 09:21:45 +0800

    fix: viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no,viewport-fit=cover"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
